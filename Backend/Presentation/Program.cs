97ac0bea4a0865dd3b84ada4147736a04b1887f2
Sofi-DCM <decristofaro.sofi@gmail.com>
2026-04-17 23:13:02 -0300

    Creacion de AppDbContext (parcial), configuracion de Connection String e inyeccion de dependencia.

--- Backend/Presentation/Program.cs
+++ Backend/Presentation/Program.cs
@@ -1,3 +1,6 @@
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Database Connection
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseLazyLoadingProxies() 
+           .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 

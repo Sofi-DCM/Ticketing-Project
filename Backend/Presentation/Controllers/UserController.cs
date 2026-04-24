01c04d536b5e7d1f5969e53af7aacea44600c2c4
Sofi-DCM <decristofaro.sofi@gmail.com>
2026-04-23 22:22:55 -0300

    Correccion de Url de controllers a "api/v1/..."

--- Backend/Presentation/Controllers/UserController.cs
+++ Backend/Presentation/Controllers/UserController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
     {

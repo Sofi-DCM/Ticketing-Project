17ed1e5d1f27b79ae2c20c5d9a2b020579b2ab7b
Jacqueline <velazquezinsaurralde@gmail.com>
2026-04-24 21:43:50 -0300

    Estructura de handlers de reserva e interfaz ICreateReservationHandler

---
+++ Backend/Application/Interfaces/Handlers/_Reservation/ICreateReservationHandler.cs
@@ -0,0 +1,15 @@
+﻿using Application.Response;
+using Application.UseCase._Reservation.Commands.CreateReservation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Handlers._Reservation
+{
+    public class ICreateReservationHandler
+    {
+            Task<ReservationResponseDto> HandleAsync(CreateReservationCommand command, CancellationToken cancellationToken);
+    }
+}

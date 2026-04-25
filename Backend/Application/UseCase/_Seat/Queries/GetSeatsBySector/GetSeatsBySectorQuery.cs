4d66b1242d65d584f7ef678bf7404c6206baf4b4
Jacqueline <velazquezinsaurralde@gmail.com>
2026-04-25 00:46:56 -0300

    Se agregó a Appliction.UseCase._Seat la estructura Query.GetSeatsBySector y el GetSeatsBySectorQuery.cs

---
+++ Backend/Application/UseCase/_Seat/Queries/GetSeatsBySector/GetSeatsBySectorQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCase._Seat.Queries.GetSeatsBySector
+{
+    public class GetSeatsBySectorQuery
+    {
+        public int SectorId { get; set; }
+    }
+}

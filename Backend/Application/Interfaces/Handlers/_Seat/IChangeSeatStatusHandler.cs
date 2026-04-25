6856e74783bd3641dbe309ab852c766619bf5b34
Jacqueline <velazquezinsaurralde@gmail.com>
2026-04-24 21:49:16 -0300

    Interfaz IChangeSeatStatusHandler y estructura de handlers de asiento

---
+++ Backend/Application/Interfaces/Handlers/_Seat/IChangeSeatStatusHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Handlers._Seat
+{
+    public interface IChangeSeatStatusHandler
+    {
+        Task HandleAsync(Guid seatId, CancellationToken ct);
+    }
+}

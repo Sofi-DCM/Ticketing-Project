822cf188db312d80fdefbd5529cb8150c7bb2229
Jacqueline <velazquezinsaurralde@gmail.com>
2026-04-25 01:19:16 -0300

    GetSeatsBySectorAsync modificado

--- Backend/Application/UseCase/_Seat/Queries/GetSeatsBySector/GetSeatsBySectorHandler.cs
+++ Backend/Application/UseCase/_Seat/Queries/GetSeatsBySector/GetSeatsBySectorHandler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Application.Interfaces.Handlers._Seat;
+using Application.Interfaces.Repositories;
+using Application.Response;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,17 @@ using System.Threading.Tasks;
 
 namespace Application.UseCase._Seat.Queries.GetSeatsBySector
 {
-    internal class GetSeatsBySectorHandler
+    public class GetSeatsBySectorHandler : IGetSeatsBySectorHandler
     {
+        private readonly ISeatRepository _repository;
+
+        public GetSeatsBySectorHandler(ISeatRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<List<SeatStatusDto>> HandleAsync (int sectorId)
+        {
+            return await _repository.GetSteasBySectorAsync(sectorId);
+        }
     }
 }

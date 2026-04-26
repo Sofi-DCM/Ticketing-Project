02b92def0d1d7801b12922f860c5e04476763b62
Sofi-DCM <decristofaro.sofi@gmail.com>
2026-04-26 05:14:39 -0300

    UserIdService ahora modificado a UserDataService.
    Cambios realizados:
    - Se importo la libreria "CryptoJS" directo en el .js .
    - Se cambio el localStorage por sesionStorage para que la sesion solo dure el navegador abierto.
    - Ahora en "u_data" tambien se guarda el name de User, se guarda un JSON{id, name}.

--- Frontend/js/services/userService.js
+++ Frontend/js/services/userService.js
@@ -1,38 +1,44 @@
 import { CONFIG } from "../configURLs/config.js";
 import { baseFetch } from "../tools/baseFetch.js";
+import CryptoJS from "https://cdn.jsdelivr.net/npm/crypto-js@4.1.1/+esm";
 
 const _secretKey = "+Vr!scCGX%@D_mh*H8rf8M%a2=l*Q$b$";
 
-export const UserIdService = {
+export const UserDataService = {
     // NOTA: debido a que el proyecto es academico y requiere un repositorio publico para correccion 
     // se guarda aqui el secretKey con un nombre obvio para encontrarlo facilmente
     // En un entorno real de produccion se usarian JWT o al menos , usando local storage, no se subiria el key al repositorio
     // sino que se usaria una variable de entorno (.env)
 
-    saveId: (id) => {
-        if(!id) return;
+    saveData: (id,name) => {
+        if(!id || !name) return;
         // Encriptar
-        const encryptedId = CryptoJS.AES.encrypt(id.toString(), _secretKey).toString();
-        localStorage.setItem('u_data', encryptedId)
+        const userData = {
+            id : id,
+            name : name
+        }
+        const jsonString = JSON.stringify(userData);
+        const encryptedData = CryptoJS.AES.encrypt(jsonString, _secretKey).toString();
+        sessionStorage.setItem('u_data', encryptedData)
     },
-    getId: () => {
-        const encryptedId = localStorage.getItem('u_data')
-        if (!encryptedId) return null;
+    getData: () => {
+        const encryptedData= sessionStorage.getItem('u_data')
+        if (!encryptedData) return null;
         
         try {
             // Intentar desencriptar
-            const bytes = CryptoJS.AES.decrypt(encryptedId, _secretKey);
-            const originalId = bytes.toString(CryptoJS.enc.Utf8);
+            const bytes = CryptoJS.AES.decrypt(encryptedData, _secretKey);
+            const jsonString = bytes.toString(CryptoJS.enc.Utf8);
             
-            // Retornar el ID plano (o null si la clave falló)
-            return originalId || null;
+            // Retornar el json de datos (o null si la clave falló)
+            return JSON.parse(jsonString) || null;
         } catch (error) {
-            console.error("Error al recuperar el ID:", error);
+            console.error("Error al recuperar datos de sesion:", error);
             return null;
         }
     },
-    clearId: () => {
-        localStorage.removeItem('u_data');
+    clearData: () => {
+        sessionStorage.removeItem('u_data');
     }
 };
 

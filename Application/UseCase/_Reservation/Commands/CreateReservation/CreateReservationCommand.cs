e4a3d05b98cc943971be0b5c14b9f59d5ca06807
Jacqueline <velazquezinsaurralde@gmail.com>
2026-04-26 21:27:02 -0300

    Eliminación de carpetas duplicadas

--- Application/UseCase/_Reservation/Commands/CreateReservation/CreateReservationCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Application.UseCase._Reservation.Commands.CreateReservation
-{
-    public class CreateReservationCommand
-    {
-        public int UserId { get; set; }
-        public Guid SeatId { get; set; }
-    }
-}
--- Application/UseCase/_Seat/Queries/GetSeatsBySector/GetSeatsBySectorQuery.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Application.UseCase._Seat.Queries.GetSeatsBySector
-{
-    public class GetSeatsBySectorQuery
-    {
-        public int SectorId { get; set; }
-    }
-}

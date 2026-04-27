704be3c08cb898ccbd7af2128be0a2bd37426379
Sofi-DCM <decristofaro.sofi@gmail.com>
2026-04-27 04:38:44 -0300

    Implementacion de sessionStorage para la navegacion del catalogo de eventos.
    Se asegura que al avanzar a ver un evento en particular o ir a loguear el usuario, se pueda retornar a la busqueda exacta donde se estaba, tanto en pagina como tipo de filtro.

--- Frontend/js/services/eventService.js
+++ Frontend/js/services/eventService.js
@@ -1,6 +1,26 @@
 import { CONFIG } from "../configURLs/config.js";
 import { baseFetch } from "../tools/baseFetch.js";
 
+export const EventDataService = {
+    saveData: (pageNumber, sortBy) => {
+        if(!pageNumber || !sortBy) return;
+
+        const data = {
+            pageNumber : pageNumber,
+            sortBy : sortBy
+        };
+        const jsonString = JSON.stringify(data);
+        sessionStorage.setItem('page_data', jsonString);
+    },
+    getData: () => {
+        const data = sessionStorage.getItem('page_data');
+        return data ? JSON.parse(data) : null;
+    },
+    clearData: () => {
+        sessionStorage.removeItem('page_data');
+    } 
+}
+
 export const EventService = {
     async GetActiveEvents(page, size, sortBy){
         const url = CONFIG.ROUTES.EVENT.GET_CATALOG(page,size,sortBy);

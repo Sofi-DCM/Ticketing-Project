0d0617fbc9863090db8055d847dbfdc82b41f4c2
Sofi-DCM <decristofaro.sofi@gmail.com>
2026-05-13 17:31:06 -0300

    Modificacion al tool timer para que siempre funcione de trasfondo incluso sin presentar visualmente

--- Frontend/js/tools/timer.js
+++ Frontend/js/tools/timer.js
@@ -1,33 +1,41 @@
 import { UserDataService } from "../services/userService.js";
+import { Toast } from "./toast.js";
 
-//Modulo global, cargado en todas las paginas html, se despierta si hay algo en el localstorage
 function initGlobalTimer() {
     const superContainer = document.querySelector(".timer-container");
     const container = document.getElementById("timerContainer");
-    if (!container) return;
 
     function update() {
-        if(UserDataService.getData() == null){
+        
+        if (UserDataService.getData() == null) {
             localStorage.removeItem('activeReservations');
-            superContainer.style.display = "none";
+            if (superContainer) superContainer.style.display = "none";
             return;
         }
         const reservations = JSON.parse(localStorage.getItem('activeReservations') || "[]");
         const now = Date.now();
-        
+
         const valid = reservations.filter(t => t.endTime > now);
-        
+
         if (valid.length !== reservations.length) {
+            const expired = reservations.filter(t => t.endTime <= now);
+
             localStorage.setItem('activeReservations', JSON.stringify(valid));
+
+            for (const t of expired) {
+                Toast.show(`Expiró reserva de asiento ${t.name}`, "info");
+            }
             window.dispatchEvent(new CustomEvent('reservationExpired'));
-            console.log("algo expiró");
         }
+        
+        if (!container || !superContainer) return; 
 
         if (valid.length === 0) {
             container.innerHTML = "";
             superContainer.style.display = "none";
             return;
         }
+
         superContainer.style.display = "flex";
         container.innerHTML = valid.map(t => {
             const diff = Math.round((t.endTime - now) / 1000);
@@ -36,7 +44,6 @@ function initGlobalTimer() {
             return `<div class="badge d-flex timer-custom m-1"><em>Asiento ${t.name} -</em><strong> ${m}:${s < 10 ? '0' : ''}${s}</strong></div>`;
         }).join('');
     }
-
     setInterval(update, 1000);
     update();
 }
